0bf1e699330cdf495308f3297a5910ff178ba37a
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-04-10 18:30:50 +0800

    Begin registerUser function

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,22 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+
+// Register User
+export const registerUser = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+  } catch (err) {}
+};

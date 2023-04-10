1ad76d8c8fd33fc8df950d007c87e92088ee1d48
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-04-10 16:41:06 +0800

    Add server directory

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import bodyParser from "bodyParser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+

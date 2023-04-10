bbc0d540ca9fc0f81fc18f88b5cf8368d22aa39e
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-04-10 18:24:20 +0800

    Create User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,53 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    min: 2,
-    max: 60,
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 60,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 60,
+    },
+    email: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 60,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+      max: 60,
+    },
+    picturePath: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: {
+      type: Number,
+      default: 0,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;

5e1d684024df8cc9e5e17a9d917682f6649b590d
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-04-10 16:55:03 +0800

    Initial configuration

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bodyParser from "bodyParser";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -9,3 +9,18 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+console.log(__filename);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
+
+dotenv.config();
+
+const app = express();
+app.use(express.json({ limit: "30mb" }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));

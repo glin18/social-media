5059865329bcadc2951f8ac0fbede87144ad41a9
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-04-11 10:47:12 +0800

    Create user friends routes

---
+++ server/controllers/users.js
@@ -0,0 +1,50 @@
+import User from "../models/User.js";
+
+// Read
+export const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+// Get User Friends
+export const getUserFriends = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    return res.status(200).json(friends);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
+  }
+};
+
+// Add remove friend
+export const addRemoveFriend = async (req, res) => {
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+
+    if (user.friends.include(friendId)) {
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filter((id) => id !== user._id);
+    } else {
+      user.friends.push(friend);
+      friend.friends.push(user);
+    }
+    user.save();
+    friend.save();
+
+    res.status(200).json({ user });
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
+  }
+};
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+// Read
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);

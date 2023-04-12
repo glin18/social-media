dbf8f5ba71354772adb486b1818c3a806b475b2a
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-04-12 10:32:09 +0800

    Setup registration form and submit handler

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,6 +3,15 @@ import { TextField, Box, Typography, Button, Avatar } from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 const Register = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
   return (
     <Box
       sx={{
@@ -13,6 +22,8 @@ const Register = () => {
         justifyContent: "center",
         gap: 3,
       }}
+      component="form"
+      onSubmit={handleSubmit}
     >
       <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
         <LockOutlinedIcon />
@@ -21,26 +32,34 @@ const Register = () => {
       <TextField
         id="outlined-basic"
         label="First Name"
+        name="firstName"
         variant="outlined"
+        required
         sx={{ maxWidth: 400, width: "100%" }}
       />
       <TextField
         id="outlined-basic"
         label="Last Name"
         variant="outlined"
+        name="lastName"
+        required
         sx={{ maxWidth: 400, width: "100%" }}
       />
       <TextField
         id="outlined-basic"
         label="Email"
         variant="outlined"
+        name="email"
+        required
         sx={{ maxWidth: 400, width: "100%" }}
       />
       <TextField
         id="outlined-basic"
         label="Password"
         type="password"
+        name="password"
         variant="outlined"
+        required
         sx={{ maxWidth: 400, width: "100%" }}
       />
       <TextField
@@ -48,9 +67,10 @@ const Register = () => {
         label="Repeat Password"
         variant="outlined"
         type="password"
+        required
         sx={{ maxWidth: 400, width: "100%" }}
       />
-      <Button variant="contained" sx={{ mb: 3 }}>
+      <Button variant="contained" sx={{ mb: 3 }} type="submit">
         Register
       </Button>
     </Box>

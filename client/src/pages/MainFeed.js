ed59a7eb4b94bb6f4c088eb26284ea8a5dad337e
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-04-13 09:31:59 +0800

    Begin creating left card

--- client/src/pages/MainFeed.js
+++ client/src/pages/MainFeed.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Grid, Paper, Avatar, Typography, Box } from "@mui/material";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
 
 const MainFeed = () => {
-  return <div>MainFeed</div>;
+  return (
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={3.5}>
+          <Paper elevation={3} sx={{ mt: 5, height: 300 }}>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <Avatar>GL</Avatar>
+              <div>
+                <Typography>Gary Lin</Typography>
+                <Typography>3 Friends</Typography>
+              </div>
+              <PersonAddIcon />
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item xs={5}></Grid>
+        <Grid item xs={3.5}></Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default MainFeed;

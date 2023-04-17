fa8ce1fca1d207475de04cba3a93014d0b9885a8
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-04-17 16:35:31 +0800

    Remove multer

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import multer from "multer";
-import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -47,20 +45,7 @@ app.use(
 // Serves static files in express
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-// File Storage
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/assets");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage });
-
 // Routes with files
-// app.post("/auth/register", upload.single("picture"), registerUser);
 app.post("/auth/register", registerUser);
 app.post("/post/create", verifyToken, createPost);
 
